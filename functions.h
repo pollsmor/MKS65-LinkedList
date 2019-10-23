49c1c2bb0d8208becf7f54a85d1ae794361e978f
pollsmor <kevinli4321@gmail.com>
2019-10-23 18:49:26 -0400

    Header file

---
+++ functions.h
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+struct Node {
+  int i;
+  struct Node *next;
+};
+
+void print_list(struct Node * node);
+
+struct Node * insert_front(struct Node * node, int i);
+
+struct Node * free_list(struct Node * node);
---
+++ main.c
@@ -0,0 +1,7 @@
+#include "functions.h"
+
+int main() {
+  struct Node n1;
+
+  return 0;
+}
